e2f6e986bb4a24ebc7ebae691f50be99c59a47de
Tharki-god <alokender018@gmail.com>
2023-03-15 01:27:46 +0530

    a little bug

--- src/listeners/MessageChecker.ts
+++ src/listeners/MessageChecker.ts
@@ -1,4 +1,4 @@
-import { MessageActions, SettingValues } from "../index";
+import { ChannelStore, MessageActions, SettingValues } from "../index";
 import { defaultSettings } from "../lib/consts";
 import { conditionalMenuItem } from "../Components/MenuItem";
 import { HBCM } from "../lib/HomeButtonContextMenuApi";
@@ -6,7 +6,9 @@ export const MessageChecker = (dispatch): void => {
   if (
     dispatch.type === "MESSAGE_CREATE" &&
     SettingValues.get("onlyMentions", defaultSettings.onlyMentions) &&
-    !MessageActions.getMessage(dispatch?.channelId, dispatch?.message?.id)?.mentioned
+    !MessageActions.getMessage(dispatch?.channelId, dispatch?.message?.id)?.mentioned &&
+    !ChannelStore.getChannel(dispatch?.channelId).isDM() &&
+    !ChannelStore.getChannel(dispatch?.channelId).isGroupDM()
   )
     return;
   HBCM.addItem("MarkAllAsRead", conditionalMenuItem());
