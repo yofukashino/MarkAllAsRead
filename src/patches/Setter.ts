02e1058bff8ab86270172de8f7729a0780f271f9
Tharki-god <alokender018@gmail.com>
2023-03-18 20:35:26 +0530

    0.9.8

--- src/patches/Setter.ts
+++ src/patches/Setter.ts
@@ -4,8 +4,7 @@ import { addListeners, removeListeners } from "../listeners/index";
 import { conditionalMenuItem, foreverMenuItem } from "../Components/MenuItem";
 import { HBCM } from "../lib/HomeButtonContextMenuApi";
 export const patchSettingSetter = (): void => {
-  PluginInjector.after(SettingValues, "set", (args) => {
-    if (args[0] !== "showForever" && args[0] !== "onlyMentions") return;
+  PluginInjector.after(SettingValues, "set", () => {
     HBCM.removeItem("MarkAllAsRead");
     removeListeners();
     HBCM.addItem(
